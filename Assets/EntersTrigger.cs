65e9c14fbf0854efb0a6c8a532fad495d6355613
Lrss <Soerensen007@gmail.com>
2017-01-09 16:04:28 +0100

    Trigger system for wheelgame.

---
+++ Assets/EntersTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntersTrigger : MonoBehaviour
+{
+
+    public Text txt;
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        txt.text = other.name;
+        Debug.Log(other.name);
+    }
+}
