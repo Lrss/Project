975973b65309a4d42e69f0797bb5017cc01d9696
Lrss <Soerensen007@gmail.com>
2017-01-09 15:28:26 +0100

    Made on playgound game.

---
+++ Assets/DisplayEasy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DisplayEasy : MonoBehaviour, IPointerClickHandler
+{
+
+    public GameObject Displayer;
+    public GameObject Cards;
+    public String ResourcePath;//Sprites\OnTripGame\PlaygroundGame
+    // Use this for initialization
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Displayer.SetActive(true);
+        var imageComp = Displayer.GetComponent<Image>();
+        string spritename = gameObject.GetComponent<Image>().sprite.name;
+        string resourcefile = ResourcePath + spritename.Substring(0, 2) + "1";
+        Debug.Log(resourcefile);
+        imageComp.overrideSprite = Resources.Load<Sprite>(resourcefile);
+        Cards.SetActive(false);
+    }
+}
