207231c02baa4cd24837c614438a691c023009b0
Lrss <Soerensen007@gmail.com>
2017-01-11 21:29:44 +0100

    Made the outside game. + rotated wheel.

---
+++ Assets/LoadSprites.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSprites : MonoBehaviour
+{
+    private int showNumber;
+    public Sprite[] sprites;
+    private Image imageDisplayer;
+
+    void OnEnable()
+    {
+        //sprites = (Sprite[])Resources.LoadAll(path, typeof(Sprite));
+        showNumber = Random.Range(0, sprites.Length);
+        imageDisplayer = gameObject.GetComponent<Image>();
+        NewImage();
+    }
+
+
+    public void NewImage()
+    {
+        showNumber++;
+        if (showNumber >= sprites.Length)
+        {
+            showNumber = 0;
+        }
+        imageDisplayer.overrideSprite = sprites[showNumber];
+    }
+}
